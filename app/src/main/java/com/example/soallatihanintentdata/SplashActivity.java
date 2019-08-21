8f3f76500c998750ff13da018579688e57e00e7b
Haikal Firdaus <haikal.firdaus.m@gmail.com>
2019-08-21 08:26:56 +0700

    Splash

---
+++ app/src/main/java/com/example/soallatihanintentdata/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.soallatihanintentdata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int loadtime = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent menu = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(menu);
+
+                this.end();
+            }
+
+            private void end() {
+
+            }
+        },loadtime);
+    }
+}
